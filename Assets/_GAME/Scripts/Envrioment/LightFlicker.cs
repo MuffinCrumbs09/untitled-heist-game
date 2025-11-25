1bd2fec880a039fb6e7eea9a46086c78f0de7300
Verity Jenkins <benniejenkins2005@outlook.com>
2025-11-25 02:42:20 +0000

    Main Menu Setup
    Started the new main menu.
    Created a random light flicker script
    Started creating UI in game, as well as animation for switching canvases

---
+++ Assets/_GAME/Scripts/Envrioment/LightFlicker.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class LightFlicker : MonoBehaviour
+{
+    [Header("Light Settings")]
+    [SerializeField, Tooltip("X = min, Y = max")] private Vector2 MinMaxTime;
+    [SerializeField, Tooltip("Starts on or off")] private bool IsOn;
+
+    private float _time;
+    private bool _isDoing;
+    private Light _light;
+
+    private void Start()
+    {
+        _light = GetComponent<Light>();
+        _light.enabled = IsOn;
+    }
+
+    private void Update()
+    {
+        if(!_isDoing)
+            StartCoroutine(ToggleLight(!IsOn));
+    }
+
+    private IEnumerator ToggleLight(bool toggle)
+    {
+        _isDoing = true;
+
+        yield return new WaitForSeconds(Random.RandomRange(MinMaxTime.x, MinMaxTime.y));
+
+        _light.enabled = toggle;
+        IsOn = toggle;
+        
+        _isDoing = false;
+    }
+}
